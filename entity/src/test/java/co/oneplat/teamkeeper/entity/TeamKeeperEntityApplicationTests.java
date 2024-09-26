493fccaad8af1f9cc6cb6933c16f71bbcb2beedc
imsejin <hackeres123@gmail.com>
2024-09-26 23:46:23 +0900

    feat: add module 'entity'

---
+++ entity/src/test/java/co/oneplat/teamkeeper/entity/TeamKeeperEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package co.oneplat.teamkeeper.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamKeeperEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
