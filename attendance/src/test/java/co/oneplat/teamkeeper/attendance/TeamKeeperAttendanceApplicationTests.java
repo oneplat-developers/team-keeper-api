54869f996cc49fac0be7596fe4cbad1ab5b5af73
imsejin <hackeres123@gmail.com>
2024-09-26 23:46:38 +0900

    feat: add module 'attendance'

---
+++ attendance/src/test/java/co/oneplat/teamkeeper/attendance/TeamKeeperAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.oneplat.teamkeeper.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamKeeperAttendanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
