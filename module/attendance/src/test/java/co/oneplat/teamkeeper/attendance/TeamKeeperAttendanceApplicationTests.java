ec7fc9d3176b36bb4aacea189e40d25c49a62074
imsejin <hackeres123@gmail.com>
2024-09-27 20:33:16 +0900

    build: fix gradle configuration

--- module/attendance/src/test/java/co/oneplat/teamkeeper/attendance/TeamKeeperAttendanceApplicationTests.java
+++ module/attendance/src/test/java/co/oneplat/teamkeeper/attendance/TeamKeeperAttendanceApplicationTests.java
@@ -3,7 +3,7 @@ package co.oneplat.teamkeeper.attendance;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class TeamKeeperAttendanceApplicationTests {
 
     @Test
