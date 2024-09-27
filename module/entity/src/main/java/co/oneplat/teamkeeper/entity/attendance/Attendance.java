29234db3a8cf1619d9160d3bc2872dc1fdbb91d2
imsejin <hackeres123@gmail.com>
2024-09-27 22:28:48 +0900

    feat: add entity 'Attendance'

---
+++ module/entity/src/main/java/co/oneplat/teamkeeper/entity/attendance/Attendance.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2024 OnePlat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.oneplat.teamkeeper.entity.attendance;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import co.oneplat.teamkeeper.entity.user.User;
+import co.oneplat.teamkeeper.entity.base.AbstractAuditableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "ATTENDANCE", uniqueConstraints = @UniqueConstraint(columnNames = {"user", "date"}))
+public class Attendance extends AbstractAuditableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ATTEND_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+    @Column(name = "ATTEND_DATE", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "ATTEND_START_TIME")
+    private LocalTime startTime;
+
+    @Column(name = "ATTEND_END_TIME")
+    private LocalDate endTime;
+
+}
