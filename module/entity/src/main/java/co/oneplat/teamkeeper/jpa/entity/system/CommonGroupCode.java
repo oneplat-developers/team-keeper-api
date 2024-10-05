e76abafa4f36334b81c6f6299b8ac0b8ffb68b7c
imsejin <hackeres123@gmail.com>
2024-10-05 21:21:52 +0900

    refactor: move entity 'CommonGroupCode'

--- module/entity/src/main/java/co/oneplat/teamkeeper/jpa/entity/system/CommonGroupCode.java
+++ module/entity/src/main/java/co/oneplat/teamkeeper/jpa/entity/system/CommonGroupCode.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package co.oneplat.teamkeeper.jpa.system;
+package co.oneplat.teamkeeper.jpa.entity.system;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
