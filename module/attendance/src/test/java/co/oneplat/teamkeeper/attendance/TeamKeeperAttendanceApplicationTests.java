395fba4d78642ab9840a1655c3533654abd0efd6
imsejin <hackeres123@gmail.com>
2024-10-14 22:32:21 +0900

    test: add test configuration on module 'entity'

--- module/attendance/src/test/java/co/oneplat/teamkeeper/attendance/TeamKeeperAttendanceApplicationTests.java
+++ module/attendance/src/test/java/co/oneplat/teamkeeper/attendance/TeamKeeperAttendanceApplicationTests.java
@@ -1,13 +1,39 @@
 package co.oneplat.teamkeeper.attendance;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Arrays;
 
-// @SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
+
+import lombok.RequiredArgsConstructor;
+
+import co.oneplat.teamkeeper.tester.annotation.SpringTestContextConfigure;
+
+@SpringBootTest
+@SpringTestContextConfigure
+// @ActiveProfiles("test")
+@RequiredArgsConstructor
+// @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class TeamKeeperAttendanceApplicationTests {
+
+    @Value("${spring.datasource.hikari.driver-class-name}")
+    private final Class<?> driverClass;
+    // @Value("${spring.profiles.active}")
+    // private final String activeProfile;
+
+    private final Environment environment;
 
     @Test
     void contextLoads() {
+        System.out.println(driverClass);
+        // System.out.println(activeProfile);
+        System.out.println(Arrays.toString(environment.getActiveProfiles()));
+        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+        System.out.println(environment.getProperty("spring.profiles.active"));
     }
 
 }
