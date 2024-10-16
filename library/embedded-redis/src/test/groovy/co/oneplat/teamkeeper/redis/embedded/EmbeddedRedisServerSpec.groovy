1255bc68f144095d0b8fc34d9465be760e416bbf
imsejin <hackeres123@gmail.com>
2024-10-16 23:16:47 +0900

    test: add test cases on library 'embedded-redis'

---
+++ library/embedded-redis/src/test/groovy/co/oneplat/teamkeeper/redis/embedded/EmbeddedRedisServerSpec.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2024 OnePlat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.oneplat.teamkeeper.redis.embedded
+
+import spock.lang.Specification
+
+import io.lettuce.core.RedisClient
+
+class EmbeddedRedisServerSpec extends Specification {
+
+    def "Checks if redis server is bound to random port"() {
+        given:
+        def server = EmbeddedRedisServer.builder().port(0).build()
+
+        when:
+        server.start()
+
+        then:
+        isBoundPort(server.ports().first)
+
+        cleanup:
+        server.stop()
+    }
+
+    def "test"() {
+        given: "Start redis server"
+        def server = EmbeddedRedisServer.builder().port(0).build()
+        server.start()
+
+        and: "Create redis client"
+        def port = server.ports().first
+        def client = RedisClient.create("redis://localhost:$port")
+
+        and: "Open a synchronous connection"
+        def connection = client.connect()
+        def syncCommands = connection.sync()
+
+        when: "Send commands to redis server"
+        def ttl = 500
+        syncCommands.psetex("foo", ttl, "bar")
+        def value = syncCommands.get("foo")
+
+        then:
+        value == "bar"
+
+        when: "Wait to expire stored data"
+        sleep(ttl)
+        value = syncCommands.get("foo")
+
+        then: "Store data is removed after time to live"
+        value == null
+
+        cleanup:
+        connection.close()
+        client.close()
+        server.stop()
+    }
+
+    // -------------------------------------------------------------------------------------------------
+
+    private static boolean isBoundPort(int port) {
+        try (ServerSocket socket = new ServerSocket(port)) {
+            false
+        } catch (IOException ignored) {
+            true
+        }
+    }
+
+}
